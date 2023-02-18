b3ffdb7dffb4aa0b0126bb42b32375cb9b395bad
gjcardonam <gjcardonam@gmail.com>
2023-02-17 20:36:34 -0500

    Not more async

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ app = Flask(__name__)
 
 
 @app.route('/tableau/<route>', methods=['POST'])
-async def controller(route):
+def controller(route):
     # Content management
     content = {
         "json": request.get_json(silent=True),
         "file": request.files
     }
 
-    return await router(route, content)
+    return router(route, content)
 
 
 if __name__ == '__main__':
